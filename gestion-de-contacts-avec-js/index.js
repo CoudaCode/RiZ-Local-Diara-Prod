296cd39afc092f2f9c7c5e2645991252b865a657
Diarassouba Mamadou <couda.dm@gmail.com>
2023-03-23 16:59:36 +0000

    deomo

--- gestion-de-contacts-avec-js/index.js
+++ gestion-de-contacts-avec-js/index.js
@@ -1,38 +1,38 @@
 // recuperer depuis localstorage
 function getContacts() {
-  return JSON.parse(localStorage.getItem('contacts'))
+  return JSON.parse(localStorage.getItem("contacts"));
 }
 
 // um tableau de contatactes
-let initialContacts = getContacts() || []
+let initialContacts = getContacts() || [];
 
-const countElement = document.querySelector('.count')
-          const table = document.querySelector('.table')
-            const tblBody = document.createElement('tbody')
+const countElement = document.querySelector(".count");
+const table = document.querySelector(".table");
+const tblBody = document.createElement("tbody");
 
 // function counter
 function setCount(count) {
-  countElement.innerHTML = count
+  countElement.innerHTML = count;
 }
 
 // ajouter dans localstorage
 function setContacts(contacts) {
-  localStorage.setItem('contacts', JSON.stringify(contacts))
+  localStorage.setItem("contacts", JSON.stringify(contacts));
 }
 
-setContacts(initialContacts)
-let contacts = getContacts()
+setContacts(initialContacts);
+let contacts = getContacts();
 
 // remplir la table
 function createTable() {
   for (let index = 0; index < contacts.length; index++) {
-    let row = document.createElement('tr')
+    let row = document.createElement("tr");
     // creer le button de supression
-    let buttonCell = document.createElement('td')
-    let deleteButton = document.createElement('button')
-    let buttonText = document.createTextNode('Supprimer')
-    deleteButton.setAttribute('class', 'delete-btn')
-    deleteButton.appendChild(buttonText)
+    let buttonCell = document.createElement("td");
+    let deleteButton = document.createElement("button");
+    let buttonText = document.createTextNode("Supprimer");
+    deleteButton.setAttribute("class", "delete-btn");
+    deleteButton.appendChild(buttonText);
 
     for (
       let element = 0;
@@ -40,137 +40,137 @@ function createTable() {
       element++
     ) {
       // ajouter les td
-      const cell = document.createElement('td')
+      const cell = document.createElement("td");
       const cellText = document.createTextNode(
         Object.values(contacts[index])[element]
-      )
-      deleteButton.setAttribute('contactPhone', contacts[index].telephone)
-      buttonCell.appendChild(deleteButton)
-      cell.appendChild(cellText)
-      row.appendChild(cell)
-      row.appendChild(buttonCell)
-      row.setAttribute('id', contacts[index].telephone)
+      );
+      deleteButton.setAttribute("contactPhone", contacts[index].telephone);
+      buttonCell.appendChild(deleteButton);
+      cell.appendChild(cellText);
+      row.appendChild(cell);
+      row.appendChild(buttonCell);
+      row.setAttribute("id", contacts[index].telephone);
     }
-    tblBody.appendChild(row)
+    tblBody.appendChild(row);
   }
-  table.appendChild(tblBody)
+  table.appendChild(tblBody);
 
-  document.body.appendChild(table)
+  document.body.appendChild(table);
 }
 
-createTable()
+createTable();
 
-let deleteButton = document.querySelectorAll('.delete-btn')
+let deleteButton = document.querySelectorAll(".delete-btn");
 
 deleteButton.forEach(function (button) {
-  button.addEventListener('click', function () {
-    const telephone = this.getAttribute('contactPhone')
+  button.addEventListener("click", function () {
+    const telephone = this.getAttribute("contactPhone");
 
-    let row = document.getElementById(telephone)
-    row.parentNode.removeChild(row)
+    let row = document.getElementById(telephone);
+    row.parentNode.removeChild(row);
 
     // enlever l'element supprimer
     let filteredContacts = contacts.filter(
       (contact) => contact.telephone !== telephone
-    )
-    contacts = filteredContacts
-    setCount(contacts.length)
-    setContacts(contacts)
-  })
-})
+    );
+    contacts = filteredContacts;
+    setCount(contacts.length);
+    setContacts(contacts);
+  });
+});
 
 // modal
-let modal = document.getElementById('contactModal')
-let modalButton = document.getElementById('addContactModalButton')
-let close = document.querySelector('.close')
+let modal = document.getElementById("contactModal");
+let modalButton = document.getElementById("addContactModalButton");
+let close = document.querySelector(".close");
 
 modalButton.onclick = function () {
-  modal.style.display = 'block'
-}
+  modal.style.display = "block";
+};
 
 close.onclick = function () {
-  modal.style.display = 'none'
-}
+  modal.style.display = "none";
+};
 
 window.onclick = function (event) {
   if (event.target == modal) {
-    modal.style.display = 'none'
+    modal.style.display = "none";
   }
-}
+};
 
 // ajouter un contact
-let addContactButton = document.querySelector('.addContactButton')
+let addContactButton = document.querySelector(".addContactButton");
 addContactButton.onclick = function (event) {
-  event.preventDefault()
-  const name = document.getElementById('name').value
-  const email = document.getElementById('email').value
-  const telephone = document.getElementById('telephone').value
+  event.preventDefault();
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const telephone = document.getElementById("telephone").value;
   if (!name || !email || !telephone) {
-    alert('merci de tout remplir')
-    return
+    alert("merci de tout remplir");
+    return;
   }
-  const newContact = { name, email, telephone }
-  contacts.push(newContact)
-  setCount(contacts.length)
-  setContacts(contacts)
+  const newContact = { name, email, telephone };
+  contacts.push(newContact);
+  setCount(contacts.length);
+  setContacts(contacts);
 
   // ajouter un tr
-  let row = document.createElement('tr')
+  let row = document.createElement("tr");
 
-  let cell0 = row.insertCell(0)
-  const cell0Text = document.createTextNode(name)
-  cell0.appendChild(cell0Text)
-  row.appendChild(cell0)
+  let cell0 = row.insertCell(0);
+  const cell0Text = document.createTextNode(name);
+  cell0.appendChild(cell0Text);
+  row.appendChild(cell0);
 
-  let cell1 = row.insertCell(1)
-  const cell1Text = document.createTextNode(email)
-  cell1.appendChild(cell1Text)
-  row.appendChild(cell1)
+  let cell1 = row.insertCell(1);
+  const cell1Text = document.createTextNode(email);
+  cell1.appendChild(cell1Text);
+  row.appendChild(cell1);
 
-  let cell2 = row.insertCell(2)
-  const cell2Text = document.createTextNode(telephone)
-  cell2.appendChild(cell2Text)
-  row.appendChild(cell2)
+  let cell2 = row.insertCell(2);
+  const cell2Text = document.createTextNode(telephone);
+  cell2.appendChild(cell2Text);
+  row.appendChild(cell2);
 
   // creer le button de supression
-  let buttonCell = document.createElement('td')
-  let deleteButton = document.createElement('button')
-  let buttonText = document.createTextNode('Supprimer')
-  deleteButton.setAttribute('class', 'delete-btn')
-  deleteButton.setAttribute('contactPhone', telephone)
-  deleteButton.appendChild(buttonText)
+  let buttonCell = document.createElement("td");
+  let deleteButton = document.createElement("button");
+  let buttonText = document.createTextNode("Supprimer");
+  deleteButton.setAttribute("class", "delete-btn");
+  deleteButton.setAttribute("contactPhone", telephone);
+  deleteButton.appendChild(buttonText);
 
   // ajouter un evenment
-  deleteButton.addEventListener('click', function () {
-    const telephone = this.getAttribute('contactPhone')
+  deleteButton.addEventListener("click", function () {
+    const telephone = this.getAttribute("contactPhone");
 
-    let row = document.getElementById(telephone)
-    row.parentNode.removeChild(row)
+    let row = document.getElementById(telephone);
+    row.parentNode.removeChild(row);
 
     // enlever l'element supprimer
     let filteredContacts = contacts.filter(
       (contact) => contact.telephone !== telephone
-    )
-    contacts = filteredContacts
-    setCount(contacts.length)
-    setContacts(contacts)
-  })
+    );
+    contacts = filteredContacts;
+    setCount(contacts.length);
+    setContacts(contacts);
+  });
 
-  buttonCell.appendChild(deleteButton)
+  buttonCell.appendChild(deleteButton);
 
-  row.appendChild(buttonCell)
+  row.appendChild(buttonCell);
 
-  row.setAttribute('id', telephone)
-  tblBody.appendChild(row)
-  table.appendChild(tblBody)
+  row.setAttribute("id", telephone);
+  tblBody.appendChild(row);
+  table.appendChild(tblBody);
 
-  document.body.appendChild(table)
+  document.body.appendChild(table);
 
   // vider les inputs
-  document.getElementById('name').value = ''
-  document.getElementById('email').value = ''
-  document.getElementById('telephone').value = ''
-  modal.style.display = 'none'
-}
+  document.getElementById("name").value = "";
+  document.getElementById("email").value = "";
+  document.getElementById("telephone").value = "";
+  modal.style.display = "none";
+};
 
-setCount(contacts.length)
+setCount(contacts.length);
--- js/superAdmin.js
+++ js/superAdmin.js
@@ -1,228 +1,245 @@
+let modal = document.getElementById("contactModal");
+let modalButton = document.getElementById("addContactModalButton");
+let close = document.querySelector(".close");
 
+modalButton.onclick = function () {
+  modal.style.display = "block";
+};
+
+close.onclick = function () {
+  modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
 let menuicn = document.querySelector(".menuicn");
-  let nav = document.querySelector(".navcontainer");
- 
-  menuicn.addEventListener("click",()=>
-  {
-      nav.classList.toggle("navclose");
-  }) 
+let nav = document.querySelector(".navcontainer");
 
+menuicn.addEventListener("click", () => {
+  nav.classList.toggle("navclose");
+});
 
 // recuperer depuis localstorage
 
-  function getAdmin() {
-  return JSON.parse(localStorage.getItem('Admin'))
+function getAdmin() {
+  return JSON.parse(localStorage.getItem("Admin"));
 }
 
-
 // um tableau de contatactes
-let initialAdmin = getAdmin() || []
+let initialAdmin = getAdmin() || [];
 
-const countElement = document.querySelector('.count')
-const table = document.querySelector('.table')
-const tblBody = document.createElement('tbody')
+const countElement = document.querySelector(".count");
+const table = document.querySelector(".table");
+const tblBody = document.createElement("tbody");
 // console.log(initialAdmin)
- 
+
 // function counter
 function setCount(count) {
-  countElement.innerHTML = count
+  countElement.innerHTML = count;
 }
 
 // ajouter dans localstorage
 function setAdmin(Admin) {
-  localStorage.setItem('Admin', JSON.stringify(Admin))
+  localStorage.setItem("Admin", JSON.stringify(Admin));
 }
 
-setAdmin(initialAdmin)
-let Admin = getAdmin()
+setAdmin(initialAdmin);
+let Admin = getAdmin();
 
 // remplir la table
-function createTable(){
+function createTable() {
   for (let index = 0; index < Admin.length; index++) {
-    let row = document.createElement('tr')
+    let row = document.createElement("tr");
     // creer le button de supression
-    let buttonCell = document.createElement('td')
-    let deleteButton = document.createElement('button')
-    let buttonText = document.createTextNode('Supprimer')
-    deleteButton.setAttribute('class', 'delete-btn')
-    deleteButton.appendChild(buttonText)
+    let buttonCell = document.createElement("td");
+    let deleteButton = document.createElement("button");
+    let buttonText = document.createTextNode("Supprimer");
+    deleteButton.setAttribute("class", "delete-btn");
+    deleteButton.appendChild(buttonText);
 
-    for (
-      let element = 0;
-      element < Object.keys(Admin[0]).length;
-      element++
-    ) {
-
-      
+    //  // creer le button de modifier
+    let UpdateCell = document.createElement("td");
+    let UpdateButton = document.createElement("button");
+    let UpdateText = document.createTextNode("Modifer");
+    UpdateButton.setAttribute("class", "modif-btn");
+    UpdateButton.appendChild(UpdateText);
+    for (let element = 0; element < Object.keys(Admin[0]).length; element++) {
       // ajouter les td
-      const cell = document.createElement('td')
+      const cell = document.createElement("td");
+
       const cellText = document.createTextNode(
         Object.values(Admin[index])[element]
-      )
+      );
 
-
-      deleteButton.setAttribute('contactPhone', Admin[index].password)
-      buttonCell.appendChild(deleteButton)
-      cell.appendChild(cellText)
-      row.appendChild(cell)
-      row.appendChild(buttonCell)
-      row.setAttribute('id', Admin[index].password)
+      // console.log('test',Object.values(Admin[index])[element])
+      // console.log(Admin[index].password);
+      // Delete
+      deleteButton.setAttribute("contactPhone", Admin[index].password);
+      UpdateButton.setAttribute("Phone", Admin[index].password)
+      buttonCell.appendChild(deleteButton);
+      cell.appendChild(cellText);
+      row.appendChild(cell);
+      row.appendChild(buttonCell);
+      row.appendChild(UpdateButton);
+      row.setAttribute("id", Admin[index].password);
 
       // console.log( Object.keys(Admin[0]));
     }
-    tblBody.appendChild(row)
+    tblBody.appendChild(row);
   }
-  table.appendChild(tblBody)
-
+  table.appendChild(tblBody);
 }
 
-createTable()
+createTable();
 
-let deleteButton = document.querySelectorAll('.delete-btn')
+let deleteButton = document.querySelectorAll(".delete-btn");
 
 deleteButton.forEach(function (button) {
-  button.addEventListener('click', function (){
-    const password = this.getAttribute('contactPhone')
-    console.log(password)
+  button.addEventListener("click", function () {
+    const password = this.getAttribute("contactPhone");
+    console.log(password);
 
-    let row = document.getElementById(password)
-   
-    row.parentNode.removeChild(row)
+    let row = document.getElementById(password);
+    console.log(row)
+    row.parentNode.removeChild(row);
     // enlever l'element supprimer
     let filteredAdmin = Admin.filter(
       (contact) => contact.password !== password
-    )
-    console.log(filteredAdmin)
-    Admin = filteredAdmin
-    setCount(Admin.length)
-    setAdmin(Admin)
-  })
-})
+    );
+    console.log(filteredAdmin);
+    Admin = filteredAdmin;
+    setCount(Admin.length);
+    setAdmin(Admin);''
+  });
+});
+
+UpdateBtn = document.querySelectorAll(".modif-btn");
+console.log(UpdateBtn);
+
+UpdateBtn.forEach(function (btn) {
+  btn.addEventListener("click", function () {
+    const password = this.getAttribute("phone");
+    console.log(password);
+    let filteredAdmin = Admin.filter(
+      (contact) => contact.password === password
+    );
+    console.log(Admin)
+    console.table(filteredAdmin);
+      
+    document.getElementById("name").value = filteredAdmin[0].name
+    document.getElementById("email").value = filteredAdmin[0].email
+    document.getElementById("password").value = filteredAdmin[0].password
+
+      modal.style.display = "block";
+   
+    
+    close.onclick = function () {
+      modal.style.display = "none";
+    };
+    
+    window.onclick = function (event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    };
+
+  });
+});
+
+
+
+
+
 
 // modal
-let modal = document.getElementById('contactModal')
-let modalButton = document.getElementById('addContactModalButton')
-let close = document.querySelector('.close')
-
-modalButton.onclick = function(){
-  modal.style.display = 'block'
-}
-
-close.onclick = function(){
-  modal.style.display = 'none'
-}
-
-window.onclick = function (event){
-  if (event.target == modal) {
-    modal.style.display = 'none'
-  }
-}
 
 // ajouter un contact
-let addContactButton = document.querySelector('.addContactButton')
+let addContactButton = document.querySelector(".addContactButton");
 addContactButton.onclick = function (event) {
-  event.preventDefault()
-  const name = document.getElementById('name').value
-  const email = document.getElementById('email').value
-  const password = document.getElementById('password').value
- 
-  if (!name || !email || !password ) {
-    alert('merci de tout remplir')
-    return 
+  event.preventDefault();
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  if (!name || !email || !password) {
+    alert("merci de tout remplir");
+    return;
   }
 
-  const newContact = { name, email, password}
-  Admin.push(newContact)
-  setCount(Admin.length)
-  setAdmin(Admin)
+  const newContact = { name, email, password };
+  Admin.push(newContact);
+  setCount(Admin.length);
+  setAdmin(Admin);
 
   // ajouter un tr
-  let row = document.createElement('tr')
+  let row = document.createElement("tr");
 
-  let cell0 = row.insertCell(0)
-  const cell0Text = document.createTextNode(name)
-  cell0.appendChild(cell0Text)
-  row.appendChild(cell0)
+  let cell0 = row.insertCell(0);
+  const cell0Text = document.createTextNode(name);
+  cell0.appendChild(cell0Text);
+  row.appendChild(cell0);
 
-  let cell1 = row.insertCell(1)
-  const cell1Text = document.createTextNode(email)
-  cell1.appendChild(cell1Text)
-  row.appendChild(cell1)
+  let cell1 = row.insertCell(1);
+  const cell1Text = document.createTextNode(email);
+  cell1.appendChild(cell1Text);
+  row.appendChild(cell1);
 
-  let cell2 = row.insertCell(2)
-  const cell2Text = document.createTextNode(password)
-  cell2.appendChild(cell2Text)
-  row.appendChild(cell2)
+  let cell2 = row.insertCell(2);
+  const cell2Text = document.createTextNode(password);
+  cell2.appendChild(cell2Text);
+  row.appendChild(cell2);
 
   // creer le button de supression
-  let buttonCell = document.createElement('td')
-  let deleteButton = document.createElement('button')
-  let buttonText = document.createTextNode('Supprimer')
-  deleteButton.setAttribute('class', 'delete-btn')
-  deleteButton.setAttribute('contactPhone', password)
-  deleteButton.appendChild(buttonText)
+  let buttonCell = document.createElement("td");
+  let deleteButton = document.createElement("button");
+  let buttonText = document.createTextNode("Supprimer");
+  deleteButton.setAttribute("class", "delete-btn");
+  deleteButton.setAttribute("contactPhone", password);
+  deleteButton.appendChild(buttonText);
 
   // Modifier
-  let UpdateCell = document.createElement('td')
-  let UpdateButton = document.createElement('button')
-  let UpdateText = document.createTextNode('Modifer')
-  UpdateButton.setAttribute('class', 'modif-btn')
-  UpdateButton.setAttribute('password', password)
-  UpdateButton.appendChild(UpdateText)
-  console.log(deleteButton)
-  console.log(UpdateButton)
-  // ajouter un evenment
-  deleteButton.addEventListener('click', function(){
-    const password = this.getAttribute('contactPhone')
+  let UpdateCell = document.createElement("td");
+  let UpdateButton = document.createElement("button");
+  let UpdateText = document.createTextNode("Modifer");
+  UpdateButton.setAttribute("class", "modif-btn");
+  UpdateButton.setAttribute("Phone", password);
+  UpdateButton.appendChild(UpdateText);
+  console.log(deleteButton);
+  console.log(UpdateButton);
 
-    let row = document.getElementById(password)
-    row.parentNode.removeChild(row)
+  // Ajouter de UpdateButtton et deleteButton a td et Tr
+  buttonCell.appendChild(deleteButton);
+  row.appendChild(buttonCell);
+  row.appendChild(UpdateButton);
+  row.setAttribute("id", password);
+  tblBody.appendChild(row);
+  table.appendChild(tblBody);
 
-    // enlever l'element supprimer
-    let filteredAdmin = Admin.filter(
-      (contact) => contact.password !== password
-    )
-   
-    Admin = filteredAdmin
-    setCount(Admin.length)
-    setAdmin(Admin)
-  })
-
- 
-
-  buttonCell.appendChild(deleteButton)
-  UpdateCell.appendChild(UpdateButton)
-  row.appendChild(buttonCell)
-  row.appendChild(UpdateCell)
-
-  row.setAttribute('id', password)
-  tblBody.appendChild(row)
-  table.appendChild(tblBody)
-  
-  
   // vider les inputs
-  document.getElementById('name').value = ''
-  document.getElementById('email').value = ''
-  document.getElementById('password').value = ''
-  modal.style.display = 'none'
-  console.log(UpdateButton)
-}
+  document.getElementById("name").value = "";
+  document.getElementById("email").value = "";
+  document.getElementById("password").value = "";
+  modal.style.display = "none";
+};
 
-setCount(Admin.length)
+setCount(Admin.length);
 
-nameAdmin = document.querySelector('.adminUser h3');
-Spadmin = document.querySelector('.adminUser h4')
-session = JSON.parse(localStorage.getItem('SessionConnect'))
+nameAdmin = document.querySelector(".adminUser h3");
+Spadmin = document.querySelector(".adminUser h4");
+session = JSON.parse(localStorage.getItem("SessionConnect"));
 
 // console.log(session)
 
-nameAdmin.textContent = session.name
-Spadmin.textContent = session.status
-Adp = document.querySelector('.dp h1')
-Adp.textContent = session.name[0].toUpperCase()
-logout = document.querySelector('.logout a')
+nameAdmin.textContent = session.name;
+Spadmin.textContent = session.status;
+Adp = document.querySelector(".dp h1");
+Adp.textContent = session.name[0].toUpperCase();
+logout = document.querySelector(".logout a");
 
-logout.addEventListener('click', ()=>{
-      localStorage.removeItem('SessionConnect')
-      console.log("c'est clear")
-})
+logout.addEventListener("click", () => {
+  localStorage.removeItem("SessionConnect");
+  console.log("c'est clear");
+});
